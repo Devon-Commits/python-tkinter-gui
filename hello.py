e9a4b476e12a9fbf223e1b5262318aa24488184e
Devon <devonvandyk@gmail.com>
2022-11-17 16:24:13 -0400

    initialize

---
+++ hello.py
@@ -0,0 +1,21 @@
+from tkinter import*
+
+root = Tk()
+root.title("GUI")           # Title at the top of the window
+root.geometry("400x400")    # Size of window
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
